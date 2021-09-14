53d3a6d83e473ef0e0c101533f7e4608b83464f4
Jean Pierre <jeanpierre101298gmail.com>
2021-09-14 12:28:33 -0500

    New red color in styles. Bg card animation ready

--- src/components/heroes/Card.vue
+++ src/components/heroes/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="card">
-    <img class="image-hero red-layer" :src="imgSrc" alt="hero image" />
-    <section class="text-container">
+    <div class="card-thumb">
+      <img :src="imgSrc" alt="hero image" />
+    </div>
+
+    <section class="card-body">
       <h2 class="name">{{ name }}</h2>
     </section>
   </div>
@@ -20,47 +23,56 @@ export default {
 </script>
 <style scoped>
 .card {
-  border-radius: 7px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   -webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   -moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   cursor: pointer;
   background-color: #151515;
+  position: relative;
+  overflow: hidden;
+}
+.card::before {
+  content: "";
+  background-color: var(--red);
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  bottom: 113px; /*becasue card-body has 120px */
+  left: 0;
+  transition: transform 0.5s ease-in-out;
 }
 
-.text-container {
+.card:hover::before {
+  transform: translate3d(0, 50%, 0);
+  -webkit-transform: translate3d(0, 50%, 0);
+}
+.card-thumb {
+  height: 350px;
+  overflow: hidden;
+}
+
+.card-thumb img {
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  object-fit: cover;
+  transition: transform 0.3s ease-out;
+}
+.card:hover .card-thumb img {
+  transform: scale(1.1);
+}
+.card-body {
   padding: 15px;
-
+  height: 120px;
   color: white;
+  position: relative;
 }
+
 .name {
-  font-size: 20px;
+  font-size: 17px;
   margin: 5px 0;
   font-weight: 400;
-}
-.image-hero {
-  width: 100%;
-  border-radius: 7px 7px 0 0;
-  height: 350px;
-  object-fit: cover;
-}
-
-.button-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  margin-top: 15px;
-  gap: 5px;
-}
-button {
-  padding: 15px 20px;
-  background-color: var(--red);
-  color: white;
-  border: none;
-  border-radius: 7px;
-  width: 100%;
   text-transform: uppercase;
-  font-weight: 500;
-  font-size: 15px;
-  text-align: center;
 }
 </style>
