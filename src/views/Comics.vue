06f8c5f535481ee9b858eb8329af600841e8dfe5
Jean Pierre <jeanpierre101298gmail.com>
2021-09-16 10:12:25 -0500

    Loading status in the comic list

--- src/views/Comics.vue
+++ src/views/Comics.vue
@@ -1,17 +1,22 @@
 <template>
   <main class="container">
     <h1 class="title">All the comics</h1>
+    <Spinner v-if="loading" />
+    <p class="error" v-if="error">There was an error</p>
     <ComicsList :comics="allComics" />
   </main>
 </template>
 <script>
 import api from "../config/api";
 import ComicsList from "../components/comics/List.vue";
+import Spinner from "../components/Spinner.vue";
 export default {
   data() {
     return {
       allComics: [],
       baseUrl: `https://gateway.marvel.com:443/v1/public/comics?ts=${api.ts}&apikey=${api.pubKey}&hash=${api.hash}`,
+      loading: false,
+      error: null,
     };
   },
   created() {
@@ -19,17 +24,23 @@ export default {
   },
   methods: {
     async getAllComics() {
+      this.loading = true;
       try {
         const res = await fetch(this.baseUrl);
         const data = await res.json();
+        this.loading = false;
+        this.error = null;
         return data.data.results;
       } catch (error) {
+        this.loading = false;
+        this.error = true;
         console.log(error);
       }
     },
   },
   components: {
     ComicsList,
+    Spinner,
   },
 };
 </script>
