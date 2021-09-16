ed728723d4b5a38aa8e06ae328121faa2a832f11
Jean Pierre <jeanpierre101298gmail.com>
2021-09-16 12:53:56 -0500

    Btn for load more comics is ready

--- src/views/Comics.vue
+++ src/views/Comics.vue
@@ -3,7 +3,19 @@
     <h1 class="title">All the comics</h1>
     <Spinner v-if="loading" />
     <p class="error" v-if="error">There was an error</p>
+
     <ComicsList :comics="allComics" />
+    <div class="action-list">
+      <button
+        v-if="limit < 100"
+        :disabled="loading"
+        class="btn"
+        v-on:click="increaseLimit"
+      >
+        More
+      </button>
+    </div>
+    <Spinner v-if="loading && limit > 20" />
   </main>
 </template>
 <script>
@@ -17,25 +29,33 @@ export default {
       baseUrl: `https://gateway.marvel.com:443/v1/public/comics?ts=${api.ts}&apikey=${api.pubKey}&hash=${api.hash}`,
       loading: false,
       error: null,
+      limit: 20,
     };
   },
   created() {
-    this.getAllComics().then((comics) => (this.allComics = comics));
+    this.setListComics(20);
   },
   methods: {
-    async getAllComics() {
+    async setListComics(limit = 20) {
       this.loading = true;
       try {
-        const res = await fetch(this.baseUrl);
+        const url = `${this.baseUrl}&limit=${limit}`;
+        const res = await fetch(url);
         const data = await res.json();
         this.loading = false;
         this.error = null;
-        return data.data.results;
+        this.allComics = data.data.results;
       } catch (error) {
         this.loading = false;
         this.error = true;
         console.log(error);
       }
+    },
+    increaseLimit() {
+      const actualLimit = this.limit;
+      if (actualLimit >= 100) return;
+      this.limit = actualLimit + 5;
+      this.setListComics(this.limit);
     },
   },
   components: {
@@ -50,5 +70,33 @@ export default {
   text-transform: capitalize;
   font-weight: 500;
   margin-bottom: 3.5rem;
+}
+.btn {
+  background-color: var(--dark);
+  color: white;
+  padding: 1rem 1.2rem;
+  text-transform: uppercase;
+  border: none;
+  cursor: pointer;
+  min-width: 12rem;
+  margin: 1rem 0;
+  transition: transform 0.3s ease-out;
+  position: relative;
+}
+@media (max-width: 768px) {
+  .btn {
+    width: 100%;
+  }
+}
+.btn:hover {
+  transform: scale(1.02);
+}
+.btn:disabled {
+  background-color: black;
+}
+.action-list {
+  text-align: center;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
 }
 </style>
